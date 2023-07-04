6beff97822993ebda496ca528e2caee63115fc25
nicoay59 <59817899+nicoay59@users.noreply.github.com>
2023-07-04 11:01:11 +0700

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ import bawah from './assets/image/bawah.png'
 function Footer() {
   return (
     <div>
-        <p style={{backgroundColor:'#FFAF00', color:'white', paddingTop:'13px',paddingBottom:'13px', marginBottom:'0rem'}}>Copyright @ 2020 Dewe Tour - Your Name - NIS. All Rights reserved</p>
+        <p style={{backgroundColor:'#FFAF00', color:'white', paddingTop:'13px',paddingBottom:'13px', marginBottom:'0rem'}}>Copyright @ 2023 Dewe Tour - Nico Ari Saputra - NIS. All Rights reserved</p>
     </div>
   )
 }
